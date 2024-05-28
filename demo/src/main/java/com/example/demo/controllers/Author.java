7b4387b9f6576a30a6cf0995e547226274717827
Anas El Hajjaji <anas.elhajjaji@ticketmaster.com>
2024-05-28 01:21:09 +0000

    enrich with more data

--- demo/src/main/java/com/example/demo/controllers/Author.java
+++ demo/src/main/java/com/example/demo/controllers/Author.java
@@ -8,7 +8,14 @@ public record Author (String id, String firstName, String lastName) {
     private static List<Author> authors = Arrays.asList(
             new Author("author-1", "Joshua", "Bloch"),
             new Author("author-2", "Douglas", "Adams"),
-            new Author("author-3", "Bill", "Bryson")
+            new Author("author-3", "Bill", "Bryson"),
+            new Author("author-4", "Author 4 first name", "Author 4 last name"),
+            new Author("author-5", "Author 5 first name", "Author 5 last name"),
+            new Author("author-6", "Author 6 first name", "Author 6 last name"),
+            new Author("author-7", "Author 7 first name", "Author 7 last name"),
+            new Author("author-8", "Author 8 first name", "Author 8 last name"),
+            new Author("author-9", "Author 9 first name", "Author 9 last name"),
+            new Author("author-10", "Author 10 first name", "Author 10 last name")
     );
 
     public static Author getById(String id) {
--- demo/src/main/java/com/example/demo/controllers/Book.java
+++ demo/src/main/java/com/example/demo/controllers/Book.java
@@ -8,7 +8,15 @@ public record Book (String id, String name, int pageCount, String authorId) {
     private static List<Book> books = Arrays.asList(
             new Book("book-1", "Effective Java", 416, "author-1"),
             new Book("book-2", "Hitchhiker's Guide to the Galaxy", 208, "author-2"),
-            new Book("book-3", "Down Under", 436, "author-3")
+            new Book("book-3", "Down Under", 436, "author-3"),
+            new Book("book-4", "Book 4", 250, "author-4"),
+            new Book("book-5", "Book 5", 250, "author-5"),
+            new Book("book-6", "Book 6", 250, "author-6"),
+            new Book("book-7", "Book 7", 250, "author-7"),
+            new Book("book-8", "Book 8", 250, "author-8"),
+            new Book("book-9", "Book 9", 250, "author-9"),
+            new Book("book-10", "Book 10", 250, "author-10")
+            
     );
 
     public static Book getById(String id) {
