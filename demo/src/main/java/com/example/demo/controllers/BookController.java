4817b04abbb69b3ad6b020317e9ee8d47d079878
Anas El Hajjaji <anas.elhajjaji@ticketmaster.com>
2024-05-28 14:55:34 +0000

    remove unused import

--- demo/src/main/java/com/example/demo/controllers/BookController.java
+++ demo/src/main/java/com/example/demo/controllers/BookController.java
@@ -4,7 +4,6 @@ import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
-import java.util.Arrays;
 import java.util.List;
 
 @Controller
